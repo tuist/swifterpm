3a4930ae6262f3ce99c3c79cbe478e5f01accbf1
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-06-09 20:48:41 +0200

    build(bazel): expose SwiftPM resolver dependency

--- third_party/nio/Package.swift
+++ third_party/nio/Package.swift
@@ -7,12 +7,17 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", exact: "2.99.0"),
         .package(url: "https://github.com/swiftlang/swift-subprocess.git", exact: "0.4.0"),
+        .package(
+            url: "https://github.com/swiftlang/swift-package-manager.git",
+            revision: "e5ac741fed39ebd16df924d3dbfa904a1c332079"
+        ),
     ],
     targets: [
         .target(
             name: "SwifterPMNIODeps",
             dependencies: [
                 .product(name: "_NIOFileSystem", package: "swift-nio"),
+                .product(name: "SwiftPMDataModel-auto", package: "swift-package-manager"),
                 .product(name: "Subprocess", package: "swift-subprocess"),
             ]
         ),
